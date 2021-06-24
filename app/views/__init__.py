1fce93695c27a8643cb611d6db5378a38d3fe822
lauslim12 <nicholasdwiarto@yahoo.com>
2021-06-24 15:04:56 +0700

    feat: Implement new data view

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -6,3 +6,8 @@ views_blueprint = Blueprint("views_blueprint", __name__)
 @views_blueprint.route("/", methods=["GET"])
 def home():
     return render_template("home.html", title="Homepage"), 200
+
+
+@views_blueprint.route("/new", methods=["GET"])
+def new():
+    return render_template("new.html", title="New"), 200
