d7df07ad37f3481502d8ae3978534247b816b327
lauslim12 <nicholasdwiarto@yahoo.com>
2021-06-24 16:34:48 +0700

    feat: Add view to edit data

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 
+from app.models import Link
+
 views_blueprint = Blueprint("views_blueprint", __name__)
 
 
@@ -11,3 +13,9 @@ def home():
 @views_blueprint.route("/new", methods=["GET"])
 def new():
     return render_template("new.html", title="New"), 200
+
+
+@views_blueprint.route("/edit/<id>", methods=["GET"])
+def edit(id):
+    link_to_edit = Link.get_one_link(id)
+    return render_template("edit.html", title="Edit", link=link_to_edit), 200
